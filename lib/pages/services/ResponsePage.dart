185da3bbb82eecf0a52e28c4ce1f145267949e3e
BLUECALF <89413138+BLUECALF@users.noreply.github.com>
2023-01-08 12:52:42 +0300

    ADD: added dictionary,response page

---
+++ lib/pages/services/ResponsePage.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+class ResponsePage extends GetView {
+  const ResponsePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("I am response Page"),
+    );
+  }
+}
