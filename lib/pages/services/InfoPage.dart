a7aab82aacab9ea5cb0526164f3f02280fd9e5b5
BLUECALF <89413138+BLUECALF@users.noreply.github.com>
2023-01-08 12:47:59 +0300

    ADD: added info page

---
+++ lib/pages/services/InfoPage.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import "package:flutter/material.dart";
+
+class InfoPage extends GetView {
+  const InfoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+     child: Text("i am info Page")
+    );
+  }
+}
